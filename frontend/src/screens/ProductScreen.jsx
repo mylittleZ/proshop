98bbc6071715dbbd52b7dfede31d848a33d109dc
mylittleZ <1020464190@qq.com>
2023-12-22 17:39:28 -0800

    feat: Implement frontend to backend request functionality

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,17 +1,28 @@
-import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
-import Rating from '../components/Rating';
-import products from '../products';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
+import axios from "axios";
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState([]);
+
   // Extracting the id parameter from the URL and assigning it to a new variable named productId
   const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
-
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
+    fetchProduct();
+    // 当依赖数组为空（[]），useEffect 只会在组件挂载时执行一次
+    // 当没有依赖数组的时候，每次组件渲染后都会执行；
+    // 当依赖数组中有变量时，代码会在组件挂载以及依赖数组中的变量改变时执行
+  }, [productId]);
   return (
     <>
-      <Link to='/' className='btn btn-light my-3'>
+      <Link to="/" className="btn btn-light my-3">
         Go Back
       </Link>
       <Row>
@@ -19,7 +30,7 @@ const ProductScreen = () => {
           <Image src={product.image} alt={product.name} fluid />
         </Col>
         <Col md={4}>
-          <ListGroup variant='flush'>
+          <ListGroup variant="flush">
             <ListGroup.Item>
               <h3>{product.name}</h3>
             </ListGroup.Item>
@@ -35,7 +46,7 @@ const ProductScreen = () => {
         </Col>
         <Col md={3}>
           <Card>
-            <ListGroup variant='flush'>
+            <ListGroup variant="flush">
               <ListGroup.Item>
                 <Row>
                   <Col>Price:</Col>
@@ -49,15 +60,15 @@ const ProductScreen = () => {
                 <Row>
                   <Col>Status:</Col>
                   <Col>
-                    {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                    {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
                   </Col>
                 </Row>
               </ListGroup.Item>
 
               <ListGroup.Item>
                 <Button
-                  className='btn-block'
-                  type='button'
+                  className="btn-block"
+                  type="button"
                   disabled={product.countInStock === 0}
                 >
                   Add To Cart

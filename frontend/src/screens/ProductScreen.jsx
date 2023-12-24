1a94c6a788379150f3f4668e77ba3265b190b061
mylittleZ <1020464190@qq.com>
2023-12-24 12:24:54 -0800

    feat: add cartSlice & addToCart reducer/action

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,9 +1,20 @@
-// import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  Form,
+} from "react-bootstrap";
 import Rating from "../components/Rating";
 import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
+import { addToCart } from "../slices/cartSlice";
+
 // import axios from "axios";
 
 const ProductScreen = () => {
@@ -20,10 +31,21 @@ const ProductScreen = () => {
   //   // 当没有依赖数组的时候，每次组件渲染后都会执行；
   //   // 当依赖数组中有变量时，代码会在组件挂载以及依赖数组中的变量改变时执行
   // }, [productId]);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { id: productId } = useParams();
+  const [qty, setQty] = useState(1);
+
   const { data: product, isLoading, error } = useGetProductDetailsQuery(
     productId
   );
+
+  const addToCartHandler = () => {
+    // dispatch(addToCart)是 在执行在cartSlice.js里导出的addToCart
+    dispatch(addToCart({ ...product, qty }));
+    navigate("/cart");
+  };
 
   return (
     <>
@@ -77,12 +99,35 @@ const ProductScreen = () => {
                       </Col>
                     </Row>
                   </ListGroup.Item>
-
+                  {/* Qty Select */}
+                  {product.countInStock > 0 && (
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Qty</Col>
+                        <Col>
+                          <Form.Control
+                            as="select"
+                            value={qty}
+                            onChange={(e) => setQty(e.target.value)}
+                          >
+                            {[...Array(product.countInStock).keys()].map(
+                              (x) => (
+                                <option key={x + 1} value={x + 1}>
+                                  {x + 1}
+                                </option>
+                              )
+                            )}
+                          </Form.Control>
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  )}
                   <ListGroup.Item>
                     <Button
                       className="btn-block"
                       type="button"
                       disabled={product.countInStock === 0}
+                      onClick={addToCartHandler}
                     >
                       Add To Cart
                     </Button>

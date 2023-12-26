4325f3262da01d581bfd6b8298530701455678cc
mylittleZ <1020464190@qq.com>
2023-12-25 18:07:25 -0800

    feat: add register user api and generateToken util

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -32,6 +32,17 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
 
+// 在存入数据库之前进行的操作
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next()
+  }
+  // generate a salt(random string, 10 is the complexities)
+  const salt = await bcrypt.genSalt(10)
+  // transfer to hash
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
 const User = mongoose.model('User', userSchema);
 
 export default User;

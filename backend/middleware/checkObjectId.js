0aa0c36fd646fdfece9c34876444a2a585a5a7df
mylittleZ <1020464190@qq.com>
2023-12-28 18:13:11 -0800

    fix: bug

--- backend/middleware/checkObjectId.js
+++ backend/middleware/checkObjectId.js
@@ -1,5 +1,6 @@
 import { isValidObjectId } from 'mongoose';
 
+// check if the `req.params.id` is a valid Mongoose ObjectId and throw an error if it is not
 function checkObjectId(req, res, next) {
   if (!isValidObjectId(req.params.id)) {
     res.status(404);
